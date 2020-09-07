df529a60e36fe7b4ccd95363b5efe662c6c1f7c9
kleddy17 <kleddy17@gmail.com>
2020-09-07 12:34:47 -0400

    second steps

--- dom_practice.js
+++ dom_practice.js
@@ -1,1 +1,58 @@
-console.log("Hello WOrld")
+
+//1
+//Get the element with the id you assigned and save it to a variable . Console.log that variable.
+//Make sure you get the element logged in the console:
+
+var myDiv = document.getElementById('scary');
+console.log('scary');
+
+
+
+
+//2
+//Get the h2 element and save it to a variable h2. Console.log that variable. 
+console.log('h2');
+
+//3
+// Using JavaScript, change the innerText of your h2 to have 3 exclamation points at the end 
+console.log('Welcome to Salem')
+document.querySelector('h2').innerText = 'Welcome to the Salem!!!'
+
+
+//4
+// Get all of the elements with the class you assigned and save them to a variable. Console.log that variable.
+var helloH4 = document.querySelector('index.h4');
+console.log(helloH4.classList[0]);
+// Make sure you get an array: 
+// And not this (a single element)
+
+
+
+//5
+//Change the style/color of the text of your ONE of your li elements to your favorite html color
+// 
+
+
+
+
+//6
+//Change the innerText of one of your list items to 'Jamboree sandwich at the Jambo-ree'
+document.querySelectorAll('li')[0].innerText = 'Jamboree sandwich at the Jambo-re'
+//Create an element and append it
+
+
+//7
+//Create a new img element using document.createElement()
+
+
+
+//8
+//Add an attribute for the source url of your new image using .setAttribute()
+
+
+//9
+//Append it to your page, somewhere, using .appendChild()
+
+
+//10
+// Check that your image has appeared
