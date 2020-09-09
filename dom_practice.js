4a3f79dc7df513230bee054d9df9185f098dceb2
kleddy17 <kleddy17@gmail.com>
2020-09-09 14:22:18 -0400

    second

--- dom_practice.js
+++ dom_practice.js
@@ -10,19 +10,17 @@ console.log('scary');
 
 
 //2
-//Get the h2 element and save it to a variable h2. Console.log that variable. 
+//Get the h2 element and save it to a variable h2. Console.log that variable. //check
+const h2 = document.querySelector('h2');
 console.log('h2');
 
 //3
-// Using JavaScript, change the innerText of your h2 to have 3 exclamation points at the end 
-console.log('Welcome to Salem')
+// Using JavaScript, change the innerText of your h2 to have 3 exclamation points at the end //check
 document.querySelector('h2').innerText = 'Welcome to the Salem!!!'
-
 
 //4
 // Get all of the elements with the class you assigned and save them to a variable. Console.log that variable.
-var helloH4 = document.querySelector('index.h4');
-console.log(helloH4.classList[0]);
+document.querySelector("index.h4")
 // Make sure you get an array: 
 // And not this (a single element)
 
@@ -30,7 +28,7 @@ console.log(helloH4.classList[0]);
 
 //5
 //Change the style/color of the text of your ONE of your li elements to your favorite html color
-// 
+// document.querySelctor('li')[1].innerText = color: Hotpink;
 
 
 
@@ -43,16 +41,18 @@ document.querySelectorAll('li')[0].innerText = 'Jamboree sandwich at the Jambo-r
 
 //7
 //Create a new img element using document.createElement()
-
-
+const pumpkin = document.createElement('img')
+// pumpkin.src = 'https://newenglandwithlove.com/wp-content/uploads/2020/02/AdobeStock_119830119-1-scaled.jpeg'
 
 //8
 //Add an attribute for the source url of your new image using .setAttribute()
-
+pumpkin.setAttribute('src','https://newenglandwithlove.com/wp-content/uploads/2020/02/AdobeStock_119830119-1-scaled.jpeg' )
 
 //9
 //Append it to your page, somewhere, using .appendChild()
-
+const container = document.querySelector('.container')
+container.appendChild(pumpkin)
 
 //10
 // Check that your image has appeared
+//It appeared on the webpage!
